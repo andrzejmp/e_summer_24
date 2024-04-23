93cf8ccb52e74af601a2b5c83e13819a933dea41
andrzejmp <andrzejmp@protonmail.com>
2024-04-23 12:39:56 +0200

    v24c

---
+++ oop/comp24b/Computer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP_in_Csharp
+{
+    class Computer
+    {
+        private string _BIOSname;
+        private string _ipadress;
+        private string _OS;
+        private bool _ON;
+        private static int _counter = 0;
+        public Computer(string bn, string ip, string os)
+        {
+            this._BIOSname = bn;
+            this._ipadress = ip;
+            this._OS = os;
+            this._ON = false;
+
+            _counter++;
+        }
+        public Computer()
+        {
+            _counter++;
+        }
+        public static int getCompsNum()
+        {
+            return _counter;
+        }
+        //proprties
+        public string BiosName
+        {
+            get { return this._BIOSname; }
+            set { this._BIOSname = value; }
+        }
+        public string IPAddress
+        {
+            get { return this._ipadress; }
+            set { this._ipadress = value; }
+        }
+
+        public string OS
+        {
+            get { return this._OS; }
+            set { this._OS = value; }
+        }
+
+        public bool ON
+        {
+            get { return this._ON; }
+            set { this._ON = value; }
+        }
+        public static string getNum()
+        {
+            Random random = new Random();
+            int num;
+            num = random.Next(1, 255);
+            return num.ToString();
+        }
+        public List<string> addresses(int N)
+        {
+            List<string> dhcpAddresses = new List<string>();
+            int count = 0;
+            string piece = "";
+
+            while (count < N)
+            {
+                piece = "10.0." + getNum() + "." + getNum();
+                if (dhcpAddresses.Contains(piece) == false)
+                {
+                    dhcpAddresses.Add(piece);
+                    count++;
+                }
+            }
+            return dhcpAddresses;
+        }
+
+            public void StartComputer(string ip)
+        {
+            IPAddress = ip;
+            ON = true;
+        }
+
+    }
+}
---
+++ oop/comp24b/Program.cs
@@ -0,0 +1,59 @@
+﻿//The version 03 of Computer Project
+using System;
+using System.Collections.Generic;
+using OOP_in_Csharp;
+using Helperspace;
+class Program
+{
+    public static void Main(string[] args)
+        {
+            List<Computer> net = new List<Computer>();
+
+             //switching ON the server
+            Console.WriteLine("\n\n------ starting server ------");
+            Computer server = new Computer();
+            server.BiosName = "SERVER";
+            server.ON = true;
+            server.IPAddress = "10.0.100.100";
+            server.OS = "Linux";
+            net.Add(server);
+            List<string> ipaddresses = new List<string>();
+            ipaddresses = server.addresses(10);
+            Console.WriteLine("Server is ON ...");
+        
+
+        //switching ON 5 computers
+        Console.WriteLine("\n\n------ starting computers in the network------");
+            const int numComps = 5;
+            for (int i = 1; i < numComps; i++)
+            {
+            Computer comp = new Computer("comp" + i.ToString(), "", "Win10");
+            net.Add(comp);
+            int last = ipaddresses.Count - 1;
+            string ip = ipaddresses[last];
+            ipaddresses.RemoveAt(last);
+            net[i].StartComputer(ip);
+            }
+            Helper.ShowComputers(net);
+
+            // shuting down computers
+            Console.WriteLine("\n\n------ shuting down computers ------");
+            Helper.ShutDownComp(net, "comp1");
+            Helper.ShutDownComp(net, "comp3");
+            Helper.ShowComputers(net);
+
+        // pinging
+            Console.WriteLine("\n\n------ pinging ------");
+            Console.Write("Enter the IP address of a computer you want to ping to: ");
+            string ip_to = Console.ReadLine();
+            if (ip_to != null)
+            {
+                Helper.ping(ip_to, net);
+            }
+
+       }  // method Main()
+    } // class Program
+
+
+
+
