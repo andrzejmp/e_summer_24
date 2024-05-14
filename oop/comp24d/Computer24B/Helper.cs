bc8ca55401bf68dba7d1acfe3644b5c52c7a87fc
andrzejmp <andrzejmp@protonmail.com>
2024-05-14 11:00:16 +0200

    Computer Project v.c

---
+++ oop/comp24d/Computer24B/Helper.cs
@@ -0,0 +1,89 @@
+﻿using OOP_in_Csharp;
+using System;
+using System.Collections.Generic;
+namespace Helperspace
+{
+    public class Helper
+    {
+
+        internal static void ShowComputers(List<Computer> network)
+        {
+            foreach (Computer comp in network)
+            {
+                string is_on = "";
+                if (comp.ON) { is_on = "ON"; }
+                Console.WriteLine("{0} {1} {2} {3}", comp.BiosName, comp.IPAddress, comp.ON, is_on );
+            }
+            Console.WriteLine("We have {0} computers!!!!", Computer.getCompsNum());
+        } // end of ShowCompters method
+
+
+        internal static void ShutDownComp(List<Computer> network, string compName)
+        {
+
+            for (int i = 0; i < network.Count; i++)
+            {
+                if (network[i].BiosName == compName)
+                {
+                    string file = @"c:\labo\log.txt";
+                    DateTime currentTime = DateTime.Now;
+                    string text = currentTime + "  " + network[i].IPAddress + " " + network[i].BiosName + "  off \n";
+                    File.AppendAllText(file, text);
+                    network[i].ON = false;
+                    network.RemoveAt(i);
+                }
+
+            }
+        } // end of ShutDownComp method
+
+        internal static void ping(string ip_to, List<Computer> network)
+        {
+            Random random = new Random();
+            int min = 10000;
+            int max = 0;
+            int time = 0;
+            double avg = 0;
+
+            bool found = false;
+            for (int i = 0; i < network.Count; i++)
+            {
+                if (ip_to == network[i].IPAddress)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (found)
+            {
+                Console.WriteLine("Pinging {0} with 32 bytes of data:", ip_to);
+                for (int i = 0; i < 4; i++)
+                {
+                    time = random.Next(1, 50);
+                    if (time > max) { max = time; }
+                    if (time < min) { min = time; }
+                    avg += time;
+                    Console.WriteLine("Reply from {0}: bytes=32 time={1}ms TTL=55", ip_to, time);
+                }
+
+                Console.WriteLine("Ping statistics for {0}", ip_to);
+                Console.WriteLine("Minimum = {0}ms Maximum = {1}ms Average = {2}ms", min, max, Math.Round(avg / 4, 0));
+            }
+            else
+            {
+                Console.WriteLine("Pinging {0} with 32 bytes of data:", ip_to);
+            }
+        }  //end of ping
+
+        internal static void displayMenu()
+        {
+            Console.WriteLine("\n\n----Menu-----");
+            Console.WriteLine("2. Start server");
+            Console.WriteLine("2. Start computers");
+            Console.WriteLine("3. Shut down computers");
+            Console.WriteLine("4. Ping tot computers");
+            Console.WriteLine("0. Quit");
+            Console.Write("\nChoose the option:  ");
+        }
+        
+}
+}
---
+++ oop/comp24d/Computer24B/Program.cs
@@ -0,0 +1,98 @@
+﻿//The version 03 of Computer Project
+using System;
+using OOP_in_Csharp;
+using Helperspace;
+using System.Net;
+class Program
+{
+    public static void Main(string[] args)
+        {
+        string file = @"c:\labo\log.txt";
+
+        DateTime currentTime = DateTime.Now;
+        //Console.WriteLine("Current local time is: " + currentTime);
+        //string text = "Computer project - log\n";
+        //File.WriteAllText(file, text);
+
+
+        List<Computer> net = new List<Computer>();
+            List<string> ipaddresses = new List<string>();
+
+            bool end = false;
+            string choice = "";
+            do
+             { 
+             Helper.displayMenu();
+             choice = Console.ReadLine();
+             Console.WriteLine("------------------------\n  ");
+             Console.Clear();
+
+            if (choice == "1")
+            {
+                //switching ON the server
+
+                Console.WriteLine("\n\n------ starting server ------");
+                Computer server = new Server();
+                server.BiosName = "SERVER";
+                server.ON = true;
+                server.IPAddress = "10.0.100.100";
+                server.OS = "Linux";
+                net.Add(server);
+                ipaddresses = server.addresses(10);
+                Console.WriteLine("Server is ON ...");
+             }
+
+            if (choice == "2")
+            {
+                //switching ON 5 computers
+                Console.WriteLine("\n\n------ starting computers in the network------");
+                const int numComps = 5;
+                for (int i = 1; i < numComps; i++)
+                {
+                    Computer comp = new Computer("comp" + i.ToString(), "", "Win10");
+                    net.Add(comp);
+                    int last = ipaddresses.Count - 1;
+                    string ip = ipaddresses[last];
+                    ipaddresses.RemoveAt(last);
+                    net[i].StartComputer(ip);
+                }
+                Helper.ShowComputers(net);
+                }
+
+
+            if (choice == "3")
+                {
+                // shuting down computers
+                Console.WriteLine("\n\n------ shuting down computers ------");
+                Helper.ShutDownComp(net, "comp1");
+                Helper.ShutDownComp(net, "comp3");
+                Helper.ShowComputers(net);
+                }
+
+
+            if (choice == "4")
+            {
+                // pinging
+                Console.WriteLine("\n\n------ pinging ------");
+                Console.Write("Enter the IP address of a computer you want to ping to: ");
+                string ip_to = Console.ReadLine();
+                if (ip_to != null)
+                {
+                    Helper.ping(ip_to, net);
+                }
+
+            }
+
+            if (choice == "0")
+                {
+                    end = true;
+                }
+          
+            
+        } while (!end);
+    }  // method Main()
+    } // class Program
+
+
+
+
